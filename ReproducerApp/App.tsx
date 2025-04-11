45cc874ecef514505030eabd3ff1301c918526db
Tero Paananen <tero.paananen@showell.com>
2025-04-11 13:00:51 +0300

    Tester

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -5,127 +5,69 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useRef } from 'react';
 import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  FlatList,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  const array = Array.from({ length: 100 }, (_, i) => i);
+  const flatListRef = useRef<FlatList<number>>(null);
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'gray',
+        gap: 100,
+      }}
+    >
+      <View style={{ marginTop: 300, backgroundColor: 'blue', height: 50 }}>
+        <FlatList
+          data={array}
+          ref={flatListRef}
+          horizontal={true}
+          scrollEventThrottle={32} // > 16 breaks onStartShouldSetResponder
+          renderItem={({ item }) => (
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                backgroundColor: 'white',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              onStartShouldSetResponder={() => {
+                // HERE tap received
+                console.log('>>>>>>>>>>> onStartShouldSetResponder', item);
+                return false;
+              }}
+              onStartShouldSetResponderCapture={() => false}
+              onMoveShouldSetResponderCapture={() => false}
+            >
+              <Text>{item}</Text>
+            </View>
+          )}
+          keyExtractor={item => item.toString()}
+        />
+      </View>
+      <Button
+        title="Scroll to 50"
+        onPress={() => {
+          flatListRef.current?.scrollToIndex({
+            index: 50,
+            animated: true, // HERE is animated
+          });
+        }}
       />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+
 
 export default App;
